1aadb547eaa9e862479f589b75c574e0b08d6686
Karl Andersson <karl.andersson1987@gmail.com>
2013-01-18 09:01:00 +0100

    CryptKey And DeCrypt Key is finished

--- Kryptor/src/com/codeslasher/kryptor/crypt/KeyNumber.java
+++ Kryptor/src/com/codeslasher/kryptor/crypt/KeyNumber.java
@@ -1,10 +1,22 @@
 package com.codeslasher.kryptor.crypt;
 
 public class KeyNumber {
-	
+	private long key;
+	public KeyNumber()
+	{
+		long time = System.currentTimeMillis();
+		if(time > 0)
+		{
+			key = time;
+		}
+		else
+		{
+			key = (long)(Math.random()*9999999);
+		}
+	}
 	
 	public long toLong()
 	{
-		return 0;
+		return key;
 	}
 }
